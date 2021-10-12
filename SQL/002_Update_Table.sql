a51498c32a74cb924f68dd3badc2624970afdeda
anis-ktari <71829876+anis-ktari@users.noreply.github.com>
2021-10-12 10:23:22 +0200

    Insert roles values

--- SQL/002_Update_Table.sql
+++ SQL/002_Update_Table.sql
@@ -1,2 +1,15 @@
-insert into Users(username,hashed_password,email,created_on,last_login) values('Clement','defdvkjkr%esfuj','clement.caton@algosup.com',now(),now());
+-- Users
+
+insert into Users(username,hashed_password,email,created_on,last_login) 
+values('Clement','defdvkjkr%esfuj','clement.caton@algosup.com',now(),now());
+
+insert into Users(username,hashed_password,email,created_on,last_login) 
+values('Ivan','rikelef565','ivan.molnar@algosup.com',now(),now());
+
+-- Roles
+
+insert into Roles(role_name) values('Full Rights');
+insert into Roles(role_name) values('Intermediate Rights');
+insert into Roles(role_name) values('Limited Rights');
+
 
